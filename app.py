9de68db59cc7dfbb217a1c17053fd57385096166
Paul Nicholsen <paulnicholsen27@gmail.com>
2023-05-21 21:19:03 -0400

    got a graph to display

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import os 
+import base64
+from io import BytesIO
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from werkzeug.utils import secure_filename
 from collections import Counter
+from matplotlib.figure import Figure
+import numpy as np 
 
 app = Flask(__name__)
 
@@ -16,32 +20,37 @@ def index():
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        f = request.files['file']
-        filename = secure_filename(f.filename)
+        if 'file' not in request.files:
+            print('No file attached in request')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            print('No file selected')
+            return redirect(request.url)
+        filename = secure_filename(file.filename)
         path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        f.save(path)
-        extract_data(path)
-        return 'file uploaded successfully'
+        file.save(path)
+        column_values = extract_data(path)
+        freq_of_first_digits = count_by_first_digit(column_values)
+        img_data = create_bar_graph(freq_of_first_digits)
+        return render_template("show_graph.html", img_data=img_data), 200
     else:
-        return "yay"
+        pass
     
 def extract_data(path):
     with open(path, 'r') as f:
         line = f.readline().split("\t")
         if line:
             column_index = get_column_index(line)
-        print("file opened")
         column_values = []
         while line:
             line = f.readline().split("\t")
-            # import ipdb; ipdb.set_trace()
             try:
                 column_values.append(line[column_index])
                 print(line[column_index])
             except IndexError:
                 break
-        print("Done")
-        count_by_first_digit(column_values)
+        return column_values
 
 def get_column_index(line, label="7_2009"):
     i = line.index(label)
@@ -52,8 +61,46 @@ def get_column_index(line, label="7_2009"):
     
 def count_by_first_digit(values):
     start_digits = [str(x)[0] for x in values]
-    print(Counter(start_digits))
+    return Counter(start_digits)
 
+def create_bar_graph(frequency_table):
+        # Generate the figure **without using pyplot**.
+    # fig = Figure()
+    # ax = fig.subplots()
+    # ax.plot([1, 2])
+    # # Save it to a temporary buffer.
+    # buf = BytesIO()
+    # fig.savefig(buf, format="png")
+    # # Embed the result in the html output.
+    # data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    # return f"<img src='data:image/png;base64,{data}'/>"
+    fig = Figure()
+    plots = fig.subplots()
+    labels, values = zip(*frequency_table.items())
+
+    indexes = np.arange(len(labels))
+    width = 1
+
+    plots.bar(indexes, values, width)
+    # plots.xticks(indexes + width * 0.5, labels)
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return f"<img src='data:image/png;base64,{data}'/>"
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# @app.route("/")
+# def hello():
+#     # Generate the figure **without using pyplot**.
+#     fig = Figure()
+#     ax = fig.subplots()
+#     ax.plot([1, 2])
+#     # Save it to a temporary buffer.
+#     buf = BytesIO()
+#     fig.savefig(buf, format="png")
+#     # Embed the result in the html output.
+#     data = base64.b64encode(buf.getbuffer()).decode("ascii")
+#     return f"<img src='data:image/png;base64,{data}'/>"
